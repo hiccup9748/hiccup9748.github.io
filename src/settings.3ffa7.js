4be88cfde137fe35107dba6ead8bb9c9084da35f
wuyu <40593247+wuyuxx@users.noreply.github.com>
2024-04-25 14:49:44 +0800

    Add files via upload

---
+++ src/settings.3ffa7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","player","enemy"],collisionMatrix:[[false],[false,false,true],[false,true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"cec13"}};
